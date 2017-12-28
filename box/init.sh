8d563f49dbb379882aee56e92050b84112311f63
Kaleo Cheng <kaleocheng@gmail.com>
2017-12-28 21:18:55 +0800

    Update vagrant box: upgrade git from 2.7 to 2.15 and remove crontab script.

--- box/init.sh
+++ box/init.sh
@@ -2,21 +2,19 @@
 set -eu -o pipefail
 
 user_path="/home/ubuntu/workspace/src/github.com/kaleocheng"
+cli_repo="https://github.com/kaleocheng/test-devops-cli.git"
 cli_path=$user_path/test-devops-cli
 mkdir -p $user_path
+git_username="Kaleo Cheng"
+git_email="kaleocheng@gmail.com"
 
 # Clone cli repo
-git clone git@github.com:kaleocheng/test-devops-cli.git $cli_path
+if [ ! -d "$cli_path" ] ; then
+    git clone $cli_repo $cli_path
+fi
 
-# Build 'test-devops-cli'
-cd $cli_path
-go build
+git config --global user.name $git_username
+git config --global user.email $git_email
 
-# Init, get the 'test-devops-site' and 'test-devops-neo' 
-./cli.sh init
-
-# Crontab
-# run 'dev' every 10 minutes
-(sudo crontab -l ; echo "*/10 * * * * cd $cli_path && ./cli.sh dev") | sort - | uniq - | sudo crontab -
-# run 'staging' every 1 hour
-(sudo crontab -l ; echo "0 * * * * cd $cli_path && ./cli.sh staging") | sort - | uniq - | sudo crontab -
+# build 'test-devops-cli' and get the 'test-devops-site' and 'test-devops-neo' 
+cd $cli_path && ./cli.sh init
